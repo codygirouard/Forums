053eddd31ddd6d492a40a82074d2c088e31fff68
Cody Girouard <codyagirouard@gmail.com>
2021-07-04 21:55:28 -0500

    Add styling for login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Feed } from './Posts';
 import { Post } from './Post';
 import { SignIn, SignUp } from './Login';
