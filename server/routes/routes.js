49c065f1e0fe282cc9365be4ceae8c589a1dd7d9
Cody Girouard <codyagirouard@gmail.com>
2021-07-04 21:53:20 -0500

    Update user login route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 import {
   addNewUser,
-  verifyUser,
+  userLogin,
   makePost,
   makeComment,
   makeReply,
@@ -9,9 +9,9 @@ import {
 } from '../controllers/controller';
 
 const routes = (app) => {
-  app.route('/be/verifyAccountInfo').get(verifyUser);
   app.route('/be/getPosts/:page?').get(getPosts);
   app.route('/be/getPost/:id').get(getPost);
+  app.route('/be/userLogin').post(userLogin);
   app.route('/be/createAccount').post(addNewUser);
   app.route('/be/newPost').post(makePost);
   app.route('/be/newComment').post(makeComment);
