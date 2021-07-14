6770826de21c4c8223cc0cfb4b19a85e0e503010
Cody Girouard <codyagirouard@gmail.com>
2021-07-13 20:17:04 -0500

    Add page to make new posts

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Feed } from './Posts';
 import { Post } from './Post';
 import { SignIn, SignUp } from './Login';
+import { NewPost } from './NewPost';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="/newpost" element={<NewPost />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
       </Routes>
