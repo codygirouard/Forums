b45eacaf35373c6964487da2b6531314192e0b82
Cody Girouard <codyagirouard@gmail.com>
2021-07-16 20:14:23 -0500

    deploy to heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import routes from './routes/routes.js';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
